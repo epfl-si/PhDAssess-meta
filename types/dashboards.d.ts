f9fce3681a6267c26768c7bf5fdb4424ff5764e4
Julien Delasoie <julien.delasoie@epfl.ch>
2023-09-14 07:57:18 +0200

    refactor: Rename and rethink fields for step

--- types/dashboards.d.ts
+++ types/dashboards.d.ts
@@ -3,20 +3,26 @@ interface StepBase {
   label: string;  // column header label
   alias?: string[]; // identify the other step that have got be finished to get a green color
   parents?: string[];  // to manage the advancing flow, meaning we know the parents should be green
+  knownAs?: string;  // if this step represents two different steps
 }
 
 interface StepWithStaticContent extends StepBase {
-  content?: string; // if we want a default content instead a colored one
-  dependsOn?: never;  // can't have the two, content or dependsOn
+  switchOnField?: never;
+  activatedOnField?: never;
+  customContent?: string; // if we want a default content instead a colored one. A empty string is valid
 }
 
-interface StepThatDependsOnField extends StepBase {
-  content?: never;   // can't have the two, content or dependsOn
-  dependsOn?: {
-    field: string;  // if we need a value in the field
-    contentOnFail?: string;  // the content printed if the field does not exists. If not set, go for the red color
-  }
+interface StepWithSwitchField extends StepBase {
+  customContent?: never;
+  activatedOnField?: never;
+  switchOnField?: string;  // this step get his green-orange status from a specific field
 }
 
-export type Step = StepWithStaticContent | StepThatDependsOnField
+interface StepWithActivatedOnField extends StepBase {
+  customContent?: never;
+  switchOnField?: never;
+  activatedOnField?: string; // define which field make this step activated, or in an unnecessary state
+}
+
+export type Step = StepWithStaticContent | StepWithSwitchField | StepWithActivatedOnField
 export type StepsDefinition = Step[]
