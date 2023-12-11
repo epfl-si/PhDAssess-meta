565d0f94348a9e5b7c8a4678b3594e39c42cd95b
Julien Delasoie <julien.delasoie@epfl.ch>
2023-12-11 19:15:18 +0100

    refactor(dashboard): Simplify the dashboard definition

--- types/dashboards.d.ts
+++ types/dashboards.d.ts
@@ -3,26 +3,18 @@ interface StepBase {
   label: string;  // column header label
   alias?: string[]; // identify the other step that have got be finished to get a green color
   parents?: string[];  // to manage the advancing flow, meaning we know the parents should be green
-  knownAs?: string;  // if this step represents two different steps
+  knownAs?: string[];  // if this step has aliases that can trigger the color, of the tasks itself or when checking for parents
 }
 
 interface StepWithStaticContent extends StepBase {
-  switchOnField?: never;
   activatedOnField?: never;
-  customContent?: string; // if we want a default content instead a colored one. A empty string is valid
-}
-
-interface StepWithSwitchField extends StepBase {
-  customContent?: never;
-  activatedOnField?: never;
-  switchOnField?: string;  // this step get his green-orange status from a specific field
+  customContent?: string; // if we want a default content instead a colored one. An empty string is valid
 }
 
 interface StepWithActivatedOnField extends StepBase {
   customContent?: never;
-  switchOnField?: never;
   activatedOnField?: string; // define which field make this step activated, or in an unnecessary state
 }
 
-export type Step = StepWithStaticContent | StepWithSwitchField | StepWithActivatedOnField
+export type Step = StepWithStaticContent | StepWithActivatedOnField
 export type StepsDefinition = Step[]
