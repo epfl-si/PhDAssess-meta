2cca3d66806281a4768d66bb710b902aec3c1cdb
Julien Delasoie <julien.delasoie@epfl.ch>
2024-10-18 10:59:39 +0200

    feat: Add new participants variables to be able to easily separate first name from last name, since the new API permit it now

--- types/participants.d.ts
+++ types/participants.d.ts
@@ -4,13 +4,18 @@ export interface ParticipantsVariables {
   programAssistantSciper: Sciper | undefined;
   programAssistantEmail: string | undefined;
   programAssistantName: string | undefined;
+  programAssistantFirstNameUsage: string | undefined;
+  programAssistantLastNameUsage: string | undefined;
 
   phdStudentSciper: Sciper | undefined;
   phdStudentEmail: string | undefined;
   phdStudentName: string | undefined;
+  phdStudentFirstNameUsage: string | undefined;
+  phdStudentLastNameUsage: string | undefined;
   /*
    * Save additional data when we crawl students info.
-   * Mainly used to generate the correct GED path
+   * Mainly used to generate the correct GED path.
+   * These are the official names
    */
   phdStudentFirstName: string | undefined;
   phdStudentLastName: string | undefined;
@@ -18,16 +23,24 @@ export interface ParticipantsVariables {
   thesisDirectorSciper: Sciper | undefined;
   thesisDirectorEmail: string | undefined;
   thesisDirectorName: string | undefined;
+  thesisDirectorFirstNameUsage: string | undefined;
+  thesisDirectorLastNameUsage: string | undefined;
 
   thesisCoDirectorSciper: Sciper | undefined;
   thesisCoDirectorEmail: string | undefined;
   thesisCoDirectorName: string | undefined;
+  thesisCoDirectorFirstNameUsage: string | undefined;
+  thesisCoDirectorLastNameUsage: string | undefined;
 
   programDirectorSciper: Sciper | undefined;
   programDirectorEmail: string | undefined;
   programDirectorName: string | undefined;
+  programDirectorFirstNameUsage: string | undefined;
+  programDirectorLastNameUsage: string | undefined;
 
   mentorSciper: Sciper | undefined;
   mentorEmail: string | undefined;
   mentorName: string | undefined;
+  mentorFirstNameUsage: string | undefined;
+  mentorLastNameUsage: string | undefined;
 }
