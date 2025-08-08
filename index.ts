54b9eaf3501fea25e66668eeeb93016c2e34c858
Julien Delasoie <julien.delasoie@epfl.ch>
2025-08-08 14:38:20 +0200

    fix: Exporting is not well received by the consumers too

--- index.ts
+++ index.ts
@@ -1,12 +1,2 @@
-// Export types
-export * from './types/fillForm/headers'
-export * from './types/customZeebeJob'
-export * from './types/dashboards'
-export * from './types/dates'
-export * from './types/inputsOutputs'
-export * from './types/notification'
-export * from './types/participants'
-export * from './types/variables'
-
 // export functionalities
 export { mergePDFs } from './src/utils'
