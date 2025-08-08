cad60da4a671566d0649536cadd9a3565a8134a9
Julien Delasoie <julien.delasoie@epfl.ch>
2025-08-08 14:27:44 +0200

    fix: Exporting type is not well received on consumers

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 // Export types
-export type * from './types/fillForm/headers'
-export type * from './types/customZeebeJob'
-export type * from './types/dashboards'
-export type * from './types/dates'
-export type * from './types/inputsOutputs'
-export type * from './types/notification'
-export type * from './types/participants'
-export type * from './types/variables'
+export * from './types/fillForm/headers'
+export * from './types/customZeebeJob'
+export * from './types/dashboards'
+export * from './types/dates'
+export * from './types/inputsOutputs'
+export * from './types/notification'
+export * from './types/participants'
+export * from './types/variables'
 
 // export functionalities
 export { mergePDFs } from './src/utils'
