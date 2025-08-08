9218a8453e25c89f51dc2ef5199bec04c44653b1
Julien Delasoie <julien.delasoie@epfl.ch>
2025-08-08 13:55:36 +0200

    fix: Try a different way to export the types

--- index.ts
+++ index.ts
@@ -1,2 +1,12 @@
-export * from './types/index'
+// Export types
+export type * from './types/fillForm/headers'
+export type * from './types/customZeebeJob'
+export type * from './types/dashboards'
+export type * from './types/dates'
+export type * from './types/inputsOutputs'
+export type * from './types/notification'
+export type * from './types/participants'
+export type * from './types/variables'
+
+// export functionalities
 export { mergePDFs } from './src/utils'
--- types/inputsOutputs.d.ts
+++ types/inputsOutputs.d.ts
@@ -2,7 +2,7 @@
  * Input-Output that transits between the builder and the notifier, as the PDF variable.
  * Used as input, then set to "" by the output. See the bpmn for details
  */
-interface _PDFBuilderToNotifInputOutput {
+export interface _PDFBuilderToNotifInputOutput {
   collaborativeReviewPDF: string | undefined;
   notAgreePDF: string | undefined;
   unsatisfactoryPDF: string | undefined;
